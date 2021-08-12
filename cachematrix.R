684a6215c12f138854ddead3b6dab1260dcedb99
webuhayo <webuhayo@up.edu.ph>
2021-08-12 22:31:14 +0800

    Draft 1 for peer review  submission

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ogMatrix <<- x
+  cachedMatrix <<- solve(x)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if(!is.null(cachedMatrix)&&ogMatrix==x){
+    return(cachedMatrix)
+  }else{
+    makeCacheMatrix(x)
+    return(cachedMatrix)
+  }
 }
